b636eccc2e3ea871454884ebb9cfb73cca352fc7
Nathan Perry <perryn1@mcmaster.ca>
2023-03-20 09:06:05 -0400

    added initial testing framework

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/EntryPoint.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/EntryPoint.java
@@ -3,8 +3,7 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a3.island.Biomes.Whittaker;
 import ca.mcmaster.cas.se2aa4.a3.island.IslandWater.LakesFactory;
-import ca.mcmaster.cas.se2aa4.a3.island.MeshAttributes.Humidity;
-import ca.mcmaster.cas.se2aa4.a3.island.MeshAttributes.Tiles;
+import ca.mcmaster.cas.se2aa4.a3.island.MeshAttributes.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +12,14 @@ public class EntryPoint {
     public static Mesh meshTest(Mesh aMesh,int lakes){
 
 
-        System.out.println("hello");
+        System.out.println("test");
         Whittaker w = new Whittaker();
 
 
         //this thing calculate the biome given a temp and humidity
-       System.out.println(w.evaluateBiome(629, 204));
-
+        System.out.println(w.evaluateBiome(629, 204));
+        TileTest test = new TileTest();
+        test.testSuite();
         Mesh lagoonMesh = LagoonIslandGenerator.LagoonMesh(aMesh);
         LakesFactory lf = new LakesFactory();
         lagoonMesh = lf.RandomLakes(lakes,lagoonMesh);
