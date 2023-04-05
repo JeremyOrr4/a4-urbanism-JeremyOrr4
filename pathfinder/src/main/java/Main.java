1821594c4be8b7b3146ef7c8aab330a427f6a1b7
Jeremy <jerorr4@yahoo.ca>
2023-04-05 11:27:00 -0400

    Added Segments list and just need to add colour F7

--- pathfinder/src/main/java/Main.java
+++ pathfinder/src/main/java/Main.java
@@ -9,8 +9,10 @@ import org.apache.batik.parser.PathArrayProducer;
 
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a3.island.MeshAttributes.Tiles;
+import ca.mcmaster.cas.se2aa4.a3.pathfinder.City.CreateCity;
 import ca.mcmaster.cas.se2aa4.a3.pathfinder.GraphADT.Edge;
 import ca.mcmaster.cas.se2aa4.a3.pathfinder.GraphADT.GraphADT;
 import ca.mcmaster.cas.se2aa4.a3.pathfinder.GraphADT.Node;
@@ -25,74 +27,35 @@ public class Main {
 
         GraphADT Graph = new GraphADT();
 
-        HashSet<Node> NodeSet = new HashSet<Node>();
-        HashSet<Edge> EdgeSet= new HashSet<Edge>();
-
-
-        int Vertex1;
-        int Vertex2;
-
-        double Vertex1XCoordinate;
-        double Vertex1YCoordinate;
-        double Vertex2XCoordinate;
-        double Vertex2YCoordinate;
-
-        Node FirstNode;
-        Node SecondNode;
-
-        Edge TempEdge;
-
-
-        for (Structs.Polygon Polygons : Mesh.getPolygonsList()){ // Creating Nodes and Edges from Graph
-            if (!(Tiles.getTileType(Polygons).equalsIgnoreCase("WATER"))){
-                for (int IndidualSegmentsID : Polygons.getSegmentIdxsList()) {
-                    Structs.Segment IndidualSegments = Mesh.getSegmentsList().get(IndidualSegmentsID);
-
-                    Vertex1 = IndidualSegments.getV1Idx();
-                    Structs.Vertex V1 = Mesh.getVertices(IndidualSegments.getV1Idx());
-                    Vertex1XCoordinate = V1.getX();
-                    Vertex1YCoordinate = V1.getY();
-                    FirstNode = new Node(Vertex1, Vertex1XCoordinate,Vertex1YCoordinate);
-                    NodeSet.add(FirstNode);
-
-                    Vertex2 = IndidualSegments.getV2Idx();
-                    Structs.Vertex V2 = Mesh.getVertices(IndidualSegments.getV2Idx());
-                    Vertex2XCoordinate = V2.getY();
-                    Vertex2YCoordinate = V2.getY();
-                    SecondNode = new Node(Vertex2, Vertex2XCoordinate,Vertex2YCoordinate);
-                    NodeSet.add(SecondNode);
-
-                    // Create edge here
-                    TempEdge = new Edge(FirstNode, SecondNode, Math.sqrt(Math.pow(Vertex2XCoordinate-Vertex1XCoordinate,2) + Math.pow(Vertex2YCoordinate-Vertex1YCoordinate,2)));
-                    Graph.AddEdge(FirstNode,SecondNode);
-                    EdgeSet.add(TempEdge);
-                }
-
-            }
-
-        }
-
-        List<Node> NodeList = new ArrayList<>(NodeSet);
-
-        // for (Node n : NodeList){
-        //     System.out.println(n.GetNodeID());
-        // }
-
-        // for (Edge e : EdgeSet){
-        //     System.out.println(e.GetWeight());
-        // }
+        CreateCity CityGraphCreator = new CreateCity();
+        List<Node> NodeList = new ArrayList<Node>();
+        NodeList = CityGraphCreator.CreateCityGraph(Mesh,Graph);
 
         ShortestPath Path = new ShortestPath();
+        List<Node> shortestDistance = Path.findPathBetweenNode(Graph, NodeList.get(2), NodeList.get(456));
+        
+        List<Segment> SegmentListOfPath = new ArrayList<Segment>();
+        SegmentListOfPath = CityGraphCreator.GetSegmentPathList(shortestDistance, Mesh, Graph);
+
+        
+        
+        
+        
+        
+        
+        
+        // for (Segment s : SegmentListOfPath) {
+        //     System.out.println("Segemnt: " + s.getV1Idx());
+        // }   
+        
+        
+        
+        // for (Node node : shortestDistance) {
+        //     System.out.println("Node List: "+ node.GetNodeID());
+        // }
 
 
-        System.out.println(NodeList.get(2).GetNodeID());
-        System.out.println(NodeList.get(4).GetNodeID());
 
-
-        List<Node> shortestDistances = Path.findPathBetweenNode(Graph, NodeList.get(2), NodeList.get(4));
-        for (Node node : shortestDistances) {
-            System.out.println(node.GetNodeID());
-        }
 
     }
 }
